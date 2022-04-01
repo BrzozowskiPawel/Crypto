b893bc791efb1978d7a8089087305a88543496cb
PawelB <pawel.brzozowski@appunite.com>
2022-04-01 17:21:11 +0200

    More info about coins

--- Crypto/View/CoinTableViewCell.swift
+++ Crypto/View/CoinTableViewCell.swift
@@ -13,6 +13,9 @@ class CoinTableViewCell: UITableViewCell {
     private let coinNameLabel = UILabel()
     private let coinPriceLabel = UILabel()
     private let namePriceStackView = UIStackView()
+    private let coinPriceChangeStackView = UIStackView()
+    private let coin24hChangeLabel = UILabel()
+    private let coin7dChangeLabel = UILabel()
     
     private var coin: Coin?
     
@@ -29,13 +32,16 @@ class CoinTableViewCell: UITableViewCell {
     
     private func setUp() {
         contentView.addSubview(coinImage)
-        contentView.addSubview(coinNameLabel)
         contentView.addSubview(namePriceStackView)
+        contentView.addSubview(coinPriceChangeStackView)
         
         setUpCoinImage()
         setUpCoinNameLabel()
         setUpCoinPriceLabel()
         setUpNamePriceStackView()
+        setUpcoinPriceChangeStackViewStackView()
+        setUp24hPriceChangePriceLabel()
+        setUp7dPriceChangePriceLabel()
     }
 
     private func setUpCoinImage() {
@@ -56,6 +62,18 @@ class CoinTableViewCell: UITableViewCell {
         coinPriceLabel.text = "USD"
     }
     
+    private func setUp24hPriceChangePriceLabel() {
+        coin24hChangeLabel.translatesAutoresizingMaskIntoConstraints = false
+        coin24hChangeLabel.text = "+20 24H"
+        coin24hChangeLabel.textAlignment = .right
+    }
+    
+    private func setUp7dPriceChangePriceLabel() {
+        coin7dChangeLabel.translatesAutoresizingMaskIntoConstraints = false
+        coin7dChangeLabel.text = "-10 7d"
+        coin7dChangeLabel.textAlignment = .right
+    }
+    
     private func setUpNamePriceStackView() {
         namePriceStackView.translatesAutoresizingMaskIntoConstraints = false
         
@@ -68,10 +86,23 @@ class CoinTableViewCell: UITableViewCell {
         namePriceStackView.spacing = 6
     }
     
+    private func setUpcoinPriceChangeStackViewStackView() {
+        coinPriceChangeStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        coinPriceChangeStackView.addArrangedSubview(coin24hChangeLabel)
+        coinPriceChangeStackView.addArrangedSubview(coin7dChangeLabel)
+        
+        coinPriceChangeStackView.axis = NSLayoutConstraint.Axis.vertical
+        coinPriceChangeStackView.distribution = UIStackView.Distribution.equalSpacing
+        coinPriceChangeStackView.alignment = UIStackView.Alignment.leading
+        coinPriceChangeStackView.spacing = 6
+    }
+    
     func addConstraints() {
         var constraints = [NSLayoutConstraint]()
         constraints += createCoinImageConstraints()
-        constraints += createNamePriceStackView()
+        constraints += createNamePriceStackViewConstraints()
+        constraints += createcoinPriceChangeStackViewConstraints()
         NSLayoutConstraint.activate(constraints)
     }
     
@@ -85,12 +116,17 @@ class CoinTableViewCell: UITableViewCell {
         ]
     }
     
-    private func createNamePriceStackView() -> [NSLayoutConstraint] {
+    private func createNamePriceStackViewConstraints() -> [NSLayoutConstraint] {
         return [
-//            coinNameLabel.leadingAnchor.constraint(equalTo: coinImage.trailingAnchor, constant: -20),
             namePriceStackView.leadingAnchor.constraint(equalTo: coinImage.trailingAnchor, constant: 20),
             namePriceStackView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
-
+        ]
+    }
+    
+    private func createcoinPriceChangeStackViewConstraints() -> [NSLayoutConstraint] {
+        return [
+            coinPriceChangeStackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -20),
+            coinPriceChangeStackView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
         ]
     }
     
@@ -99,6 +135,8 @@ class CoinTableViewCell: UITableViewCell {
         if let coin = coin {
             coinNameLabel.text = coin.name
             coinPriceLabel.text = "\(coin.price_usd) $"
+            coin24hChangeLabel.text = "\(coin.percent_change_24h)% 24h"
+            coin7dChangeLabel.text = "\(coin.percent_change_7d) 7d"
             if let coinImg =  UIImage(named: coin.symbol.lowercased()){
                 coinImage.image = coinImg
             }
